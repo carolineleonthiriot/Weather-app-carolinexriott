8f914e6cc0b16ffc88388206a222517be6916d76
Caroline T <106573147+carolineleonthiriot@users.noreply.github.com>
2022-06-20 20:44:07 -0500

    New upload

---
+++ script.js
@@ -0,0 +1,113 @@
+let now = new Date();
+
+let h3 = document.querySelector("h3");
+
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+h3.innerHTML = `${day} ${hours}:${minutes}`;
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "32f6ab694fdb28dfd287b24b4bf7a4d3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "8db36d6c9d3df2e0e030fad1f1bc7040";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Chicago");
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-input");
+
+  let h2 = document.querySelector("h2");
+
+  h2.innerHTML = `${searchInput.value}`;
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+let celsius = document.querySelector(".celsius");
+let fahrenheit = document.querySelector(".fahrenheit");
+
+function makeCelsius() {
+  let temp = document.querySelector("#temp");
+  temp.innerHTML = "16";
+}
+
+function makeFahrenheit() {
+  let temp = document.querySelector("#temp");
+  temp.innerHTML = "61";
+}
+
+celsius.addEventListener("click", makeCelsius);
+fahrenheit.addEventListener("click", makeFahrenheit);
+
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80,
+  },
+  tokyo: {
+    temp: 17.3,
+    humidity: 50,
+  },
+  lisbon: {
+    temp: 30.2,
+    humidity: 20,
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100,
+  },
+  moscow: {
+    temp: -5,
+    humidity: 20,
+  },
+};
