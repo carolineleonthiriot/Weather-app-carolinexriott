0b4ad1713b27c496d0e530b229d19aeacb17157f
Caroline T <106573147+carolineleonthiriot@users.noreply.github.com>
2022-07-09 12:17:06 -0500

    New color design

--- script.js
+++ script.js
@@ -116,13 +116,3 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("Paris");
-
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let fahrenheintTemperature = (14 * 9) / 5 + 32;
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-let fahrenheitLink = document.querySelector("fahrenheint-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
